d3e27b8c5013de120eeb5931626385db3fdef43b
Kristina Trofimova <aktrofimova@mail.ru>
2021-02-19 19:49:41 +0300

    qt-client

---
+++ src/qt-client/login.cpp
@@ -0,0 +1,36 @@
+#include "login.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+   /* QWidget *LogInWidget = new QWidget(this);
+        LogInWidget->setStyleSheet("background-color: #E6D1D1");
+        setCentralWidget(LogInWidget);*/
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_logIn_clicked()
+{
+    QString username = ui ->lineEdit_username -> text();
+    QString password = ui ->lineEdit_password -> text();
+
+    /*if (password == "password" && username == "user") {
+        ui -> label_HelloUser -> setText("Ура! Вы вошли");
+        todoList = new ToDoList();
+        todoList -> show();
+        close();
+    } else {
+        ui -> label_HelloUser -> setText("Неверный логин или пароль. Попробуйте снова");
+    }*/
+    todoList = new ToDoList();
+    todoList->show();
+}
