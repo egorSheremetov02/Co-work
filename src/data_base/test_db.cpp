d66ea74b8cafba3203467538e09d53c77ea4692e
irzuk <irinazel2002@gmail.com>
2021-02-27 18:11:25 +0300

    add functions for DB

---
+++ src/data_base/test_db.cpp
@@ -0,0 +1,11 @@
+#include "../shared/db_connection.h"
+
+int main(){
+  //std::cout<<auth("admin","qwerty")->full_name<<std::endl;
+
+  std::vector<Task> data=get_tasks(1);
+
+for(auto & z: data){
+  std::cout<<z.name<<std::endl;}
+
+}
