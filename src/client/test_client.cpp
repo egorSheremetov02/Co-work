c1575672eaafb81cb2b893d7f689553dfab0118b
Egor Sheremetov <egor.sheremetov@yandex.ru>
2021-03-06 13:23:34 +0300

    Fixed client simultaneous connection attempts (that corrupted messages that were sent later to tcp-socket)

--- src/client/test_client.cpp
+++ src/client/test_client.cpp
@@ -36,51 +36,58 @@ void do_read() {
                         << std::endl;
             });
 }
+void authenticate() {
+    const asio::ip::basic_endpoint<tcp> &endpoint = tcp::endpoint(asio::ip::address::from_string("127.0.0.1"),
+                                                                  3030);
+
+    socket1.async_connect(endpoint, [&](asio::error_code const &ec) {
+        std::cout << "Successfully connected" << std::endl;
+        std::string passcode;
+        std::string login;
+        std::cout << "Enter login: ";
+        std::cin >> login;
+        std::cout << "Enter password: ";
+        std::cin >> passcode;
+        RequestFormat<AuthReqDTO> auth_request;
+        auth_request.resource = "authentication";
+        auth_request.data = {login, passcode};
+        json j = auth_request;
+        std::string str_auth_request = j.dump();
+        socket1.async_write_some(asio::buffer(str_auth_request.data(), str_auth_request.size()),
+                                 [&](asio::error_code const &ec, std::size_t) {
+                                     if (ec) return;
+                                     auth_response.resize(1024);
+                                     socket1.async_read_some(
+                                             asio::buffer(auth_response.data(), auth_response.size()),
+                                             [&](asio::error_code const &ec, std::size_t len) {
+                                                 std::cout << "Successfully read data from server: "
+                                                           << auth_response << " of size " << len
+                                                           << std::endl;
+                                                 json json_auth_response = json::parse(
+                                                         auth_response.substr(0, len));
+                                                 auto response = json_auth_response.get<ResponseFormat<User>>();
+                                                 if (response.error.empty()) {
+                                                     std::cout << "Successfully authenticated to server"
+                                                               << std::endl;
+                                                     do_read();
+                                                 } else {
+                                                     socket1.close();
+                                                     std::cout
+                                                             << "Unauthorized auth_request to authorized resource"
+                                                             << std::endl;
+                                                     authenticate();
+                                                 }
+                                             });
+                                 });
+    });
+}
 
 int main() {
 
     const asio::ip::basic_endpoint<tcp> &endpoint = tcp::endpoint(asio::ip::address::from_string("127.0.0.1"),
                                                                   3030);
-    {
-        socket1.async_connect(endpoint, [&](asio::error_code const &ec) {
-            std::cout << "Successfully connected" << std::endl;
-            std::string passcode;
-            std::string login;
-            std::cout << "Enter login: ";
-            std::cin >> login;
-            std::cout << "Enter password: ";
-            std::cin >> passcode;
-            RequestFormat<AuthReqDTO> auth_request;
-            auth_request.resource = "authentication";
-            auth_request.data = {login, passcode};
-            json j = auth_request;
-            std::string str_auth_request = j.dump();
-            socket1.async_write_some(asio::buffer(str_auth_request.data(), str_auth_request.size()),
-             [&](asio::error_code const &ec, std::size_t) {
-                 if (ec) return;
-                 auth_response.resize(1024);
-                 socket1.async_read_some(
-                 asio::buffer(auth_response.data(), auth_response.size()),
-                 [&](asio::error_code const &ec, std::size_t len) {
-                     std::cout << "Successfully read data from server: "
-                               << auth_response << " of size " << len
-                               << std::endl;
-                     json json_auth_response = json::parse(
-                             auth_response.substr(0, len));
-                     auto response = json_auth_response.get<ResponseFormat<User>>();
-                     if (response.error.empty()) {
-                         std::cout << "Successfully authenticated to server"
-                                   << std::endl;
-                         do_read();
-                     } else {
-                         std::cout
-                                 << "Unauthorized auth_request to authorized resource"
-                                 << std::endl;
-                     }
-                 });
-             });
-        });
-    }
+
+    authenticate();
 
     service.run();
 }
