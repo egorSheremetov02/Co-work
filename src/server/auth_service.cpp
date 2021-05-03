d8e1d4030cbc2cf17e762d718ed229cfbcfb4daf
Egor Sheremetov <egor.sheremetov@yandex.ru>
2021-05-03 12:58:25 +0300

    set pre-commit as executable

--- src/server/auth_service.cpp
+++ src/server/auth_service.cpp
@@ -1,19 +1,20 @@
 //
 // Created by egor on 27.02.2021.
 //
+#include "auth_service.h"
 #include <optional>
-#include "src/shared/structures.h"
 #include "app_db_connection.h"
 #include "src/shared/request.h"
-#include "auth_service.h"
+#include "src/shared/structures.h"
 
 std::optional<User> AuthService::validate(const AuthReqDTO &payload) const {
     if (get_app_db().auth(payload.login, payload.password)) {
-        User                                u;
-        u.          account_name = "egor.suvorov";
-        u. role_in_system = ADMIN;
-        u.full_name = "Egor Suvorov"; u.id = 1; return std::optional<User>{u};
+        User u;
+        u.account_name = "egor.suvorov";
+        u.role_in_system = ADMIN;
+        u.full_name = "Egor Suvorov";
+        u.id = 1;
+        return std::optional<User>{u};
     }
     return std::optional<User>{};
 }
-
