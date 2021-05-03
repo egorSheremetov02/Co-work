86a77569d3df4acb574a454dbcfe71e41ebef5d0
Egor Sheremetov <egor.sheremetov@yandex.ru>
2021-05-03 13:36:13 +0300

    github-action script for checking formatting

--- scripts/formatting.sh
+++ scripts/formatting.sh
@@ -3,4 +3,4 @@
 echo "Linting your code..."
 
 # shellcheck disable=SC2044
-for file in $(find ./src -iname '*.cpp' -or -iname '*.c' -or -iname '*.h' -and -not -iname 'doctest.h' ); do clang-format -style=google -i "$file" ; done
+for file in $(find . -iname '*.cpp' -or -iname '*.c' -or -iname '*.h' -or -iname '*.hpp' -and -not -iname 'doctest.h' ); do diff -u <(cat "$file") <(clang-format "$file") || exit 1; done
